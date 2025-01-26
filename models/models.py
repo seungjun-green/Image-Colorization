e03c389025c896af96d293695c7c4ae381e5269a
seungjun-green <lsj3285007@gmail.com>
2025-01-27 01:16:43 +0900

    add AttentionUNet

--- models/models.py
+++ models/models.py
@@ -39,6 +39,53 @@ class Upsample(nn.Module):
     def forward(self, x):
         return self.block(x)
 
+
+class PatchGANDiscriminator(nn.Module):
+    def __init__(self, in_channels=1, target_channels=2):
+        super().__init__()
+        combined_channels = in_channels + target_channels
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(combined_channels, 64, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=4, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+        self.final = nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=1)
+
+    def forward(self, input_image, target_image):
+        """
+        input: (N, 3, H, W)
+        output: (N, )
+        Args:
+            input_image (N, C_in, H, W)
+            target_image: (N, C_out, H, W)
+        """
+        x = torch.cat([input_image, target_image], dim=1)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.final(x)
+        return x
+    
 class UNetGenerator(nn.Module):
     def __init__(self, in_channels=1, out_channels=2):
         super().__init__()
@@ -99,52 +146,106 @@ class UNetGenerator(nn.Module):
         out = self.tanh(out)
 
         return out
-
-class PatchGANDiscriminator(nn.Module):
-    def __init__(self, in_channels=1, target_channels=2):
+    
+class AttenionGate(nn.Module):
+    def __init__(self, channels):
         super().__init__()
-        combined_channels = in_channels + target_channels
-
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(combined_channels, 64, kernel_size=4, stride=2, padding=1),
-            nn.LeakyReLU(0.2, inplace=True),
-        )
-
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2, inplace=True),
-        )
-
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.2, inplace=True),
-        )
-
-        self.layer4 = nn.Sequential(
-            nn.Conv2d(256, 512, kernel_size=4, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.2, inplace=True),
-        )
-
-        self.final = nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=1)
-
-    def forward(self, input_image, target_image):
-        """
-        input: (N, 3, H, W)
-        output: (N, )
-        Args:
-            input_image (N, C_in, H, W)
-            target_image: (N, C_out, H, W)
-        """
-        x = torch.cat([input_image, target_image], dim=1)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = self.final(x)
+        self.channels = channels
+        self.conv1 = nn.Conv2d(channels,channels / 2, 1)
+        self.conv2 = nn.Conv2d(channels / 2, 1, 1)
+    
+    def forward(self, x):
+        '''
+        input and output shape are same
+        '''
+        original_x = x
+        # Step1. get the attention mask
+        # x: (N, channel, H, W)
+        x = self.conv1(x)
+        x = torch.nn.functional.relu(x)
+        x = self.conv2(x) # (N, 1, H, W)
+        x = self.sigmoid(x)
+        
+        # Step2. do the element wise multiplication
+        x = original_x * x
         return x
+
+class AttentionUNetGenerator(nn.Module):
+    def __init__(self, in_channels=1, out_channels=2):
+        super().__init__()
+        self.down1 = Downsample(in_channels, 64, apply_batchnorm=False)
+        self.down2 = Downsample(64, 128)
+        self.down3 = Downsample(128, 256)
+        self.down4 = Downsample(256, 512)
+        self.down5 = Downsample(512, 512)
+        self.down6 = Downsample(512, 512)
+        self.down7 = Downsample(512, 512)
+        self.down8 = Downsample(512, 512, apply_batchnorm=False)
+
+        self.up1 = Upsample(512, 512, apply_dropout=True)
+        self.up2 = Upsample(1024, 512, apply_dropout=True)
+        self.up3 = Upsample(1024, 512, apply_dropout=True)
+        self.up4 = Upsample(1024, 512)
+        self.up5 = Upsample(1024, 256)
+        self.up6 = Upsample(512, 128)
+        self.up7 = Upsample(256, 64)
+
+        self.final = nn.ConvTranspose2d(128, out_channels, kernel_size=4, stride=2, padding=1)
+        self.tanh = nn.Tanh()
+        
+        self.ag1 = AttenionGate(1024)
+        self.ag2 = AttenionGate(1024)
+        self.ag3 = AttenionGate(1024)
+        self.ag4 = AttenionGate(1024)
+        self.ag5 = AttenionGate(512)
+        self.ag6 = AttenionGate(256)
+        self.ag7 = AttenionGate(128)
+
+    def forward(self, x):
+        # encoder
+        d1 = self.down1(x) # (N, 64, H/2, W/2)
+        d2 = self.down2(d1) # (N, 128, H/4, W/4)
+        d3 = self.down3(d2) # (N, 256, H/8, W/8)
+        d4 = self.down4(d3) # (N, 512, H/16, W/16)
+        d5 = self.down5(d4) # (N, 512, H/32, W/32)
+        d6 = self.down6(d5) # (N, 512, H/64, W/64)
+        d7 = self.down7(d6) # (N, 512, H/128, W/128)
+        d8 = self.down8(d7) # (N, 512, H/256, W/256)
+        
+
+        # decoder
+        u1 = self.up1(d8) # (N, 512, H/128, W/128)
+        u1 = torch.cat([u1, d7], dim=1) # (N, 1024, H/128, W/128)
+        u1 = self.ag1(u1)
+
+        u2 = self.up2(u1)
+        u2 = torch.cat([u2, d6], dim=1)
+        u2 = self.ag2(u2)
+
+        u3 = self.up3(u2)
+        u3 = torch.cat([u3, d5], dim=1)
+        u3 = self.ag2(u3)
+
+        u4 = self.up4(u3)
+        u4 = torch.cat([u4, d4], dim=1)
+        u4 = self.ag2(u4)
+
+        u5 = self.up5(u4)
+        u5 = torch.cat([u5, d3], dim=1)
+        u5 = self.ag2(u5)
+
+        u6 = self.up6(u5)
+        u6 = torch.cat([u6, d2], dim=1)
+        u6 = self.ag2(u6)
+
+        u7 = self.up7(u6)
+        u7 = torch.cat([u7, d1], dim=1)
+        u7 = self.ag2(u7)
+
+        out = self.final(u7)
+        out = self.tanh(out)
+
+        return out
 class ResNetUNetGenerator(nn.Module):
     def __init__(self, in_channels=1, out_channels=2):
         super().__init__()
--- tests/model_tests.py
+++ tests/model_tests.py
@@ -9,15 +9,19 @@ def test_generator():
     
     gen1 = UNetGenerator()
     gen2 = ResNetUNetGenerator()
+    gen3 = AttentionUNetGenerator()
     
     sample_output1 = gen1(sample_input)
     sample_output2 = gen2(sample_input)
+    sample_output3 = gen3(sample_input)
     
     expected_shape = (1, 2, 256, 256)
     
     # check whether all has shape of (N, 2, 256, 256)
     assert sample_output1.shape == expected_shape, f"gen1 output shape {sample_output1.shape} does not match expected {expected_shape}"
     assert sample_output2.shape == expected_shape, f"gen2 output shape {sample_output2.shape} does not match expected {expected_shape}"
+    assert sample_output2.shape == expected_shape, f"gen3 output shape {sample_output3.shape} does not match expected {expected_shape}"
+    
     
     print("All generators produce outputs with the expected shape:", expected_shape)
     
